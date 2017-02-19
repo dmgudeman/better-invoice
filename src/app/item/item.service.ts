754eae36ece722ec80487744f1b815d378e165de
davidmgudeman <davidmgudeman@gmail.com>
2017-02-19 15:03:33 -0800

    started hooking up back end to item field in database

---
+++ src/app/item/item.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Http }       from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ItemService {
+	private _url = "http://localhost:3000/itms";
+
+	constructor(private _http: Http){
+	}
+
+	getItems(){
+		return this._http.get(this._url)
+			.map(res => res.json());
+	}
+    
+    getItem(itemId){
+		return this._http.get(this.getItemUrl(itemId))
+			.map(res => res.json());
+	}
+    
+    addItem(item){
+		return this._http.post(this._url, JSON.stringify(item))
+			.map(res => res.json());
+	}
+    
+    updateItem(item){
+		return this._http.put(this.getItemUrl(item.id), JSON.stringify(item))
+			.map(res => res.json());
+	}
+    
+    deleteItem(itemId){
+		return this._http.delete(this.getItemUrl(itemId))
+			.map(res => res.json());
+	}
+    
+    private getItemUrl(itemId){
+		return this._url + "/" + itemId;
+	}
+}
