7a269298a6203e1624adaba54942555ec3b33912
davidmgudeman <davidmgudeman@gmail.com>
2017-02-20 20:07:42 -0800

    new-company: updated the model and view so the formControlNames match the variables on the backend

--- src/app/company/new-company/new-company.component.ts
+++ src/app/company/new-company/new-company.component.ts
@@ -19,76 +19,23 @@ export class NewCompanyComponent implements OnInit {
   // private paymentTerms;
   // private active;
   myform : FormGroup;
-  public colorTags =[
-    {value: 'brown', display:''},
-    {value: 'blue', display:''},
-    {value: 'red', display:''},
-    {value: 'green', display:''},
-    {value: 'yellow', display:''},
-    {value: 'purple', display:''},
-  ]
+  
   constructor(
            private _companyService: CompanyService,
            private _router:Router) { }
 
   ngOnInit() {
       this.myform = new FormGroup({
-        businessName: new FormControl(),
-        colorTags: new FormControl(),
-        hourlyPay: new FormControl(),
+        name: new FormControl(),
+        color: new FormControl(),
+        hourly: new FormControl(),
         paymentTerms: new FormControl(),
         active: new FormControl(),
       })
   }
    
-  addCompany(){ 
-   
-    
-      console.log( this.myform.value.colorTags );
   
-    
-
-  //  this._companyService.addCompany(company)
-
-      //  .subscribe( (data) => 
-      //      console.log(data),
-      //     //  {this._companyService.getCompanies(); 
-      //     //    return true},
-      //      error=> { console.error("Error saving company!");
-      //                return Observable.throw(error);}
-      // );
-  }
-
   onSubmit(form) {
       console.log("Form Submitted!");
     console.log( this.myform.value )}
 }
-// save(){
-//         var result;
-        
-//         if (this.company.id) 
-        
-//             result = this._companyService.updateCompany(this.company);
-//         else
-//             result = this._companyService.addCompany(this.company)
-            
-// 		result.subscribe(x => {
-//             // Ideally, here we'd want:
-//             // this.form.markAsPristine();
-//             this._router.navigate(['companies']);
-//         });
-// 	}
-   
-// }
-//From userForm
-
-//From user.service
-//  addUser(user){
-// 		return this._http.post(this._url, JSON.stringify(user))
-// 			.map(res => res.json());
-// 	}
-    
-//     updateUser(user){
-// 		return this._http.put(this.getUserUrl(user.id), JSON.stringify(user))
-// 			.map(res => res.json());
-// 	}
