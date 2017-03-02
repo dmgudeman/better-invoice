47d58bb7650f7c42268587d62d13f7ac57ecd4e4
davidmgudeman <davidmgudeman@gmail.com>
2017-03-02 09:24:03 -0800

    the refactoring of companys into companies was accomplished now can get rid of companys, and companies copy.

--- src/app/company/companies/companies.component.ts
+++ src/app/company/companies/companies.component.ts
@@ -10,11 +10,12 @@ import { Observable } from 'RXJS/Observable';
 import { CompanyCardComponent } from '../company-card/company-card.component';
 
 @Component({
-  selector: 'companies',
-  templateUrl: 'companies.component.html',
-  styleUrls: ['companies.component.css']
+  selector: 'app-companies',
+  templateUrl: './companies.component.html',
+  styleUrls: ['./companies.component.css']
 })
 export class CompaniesComponent implements OnInit {
+
   class: any;
   companies: Observable<Company[]>;
   invoice: Invoice;
@@ -25,79 +26,22 @@ export class CompaniesComponent implements OnInit {
   paymentTerms:number;
   active:boolean;
   editing:boolean = false;
+ 
   
   constructor(
               private _companyService: CompanyService,
               private _invoiceService: InvoiceService,
-              private router:Router) { };
+              private router:Router) { 
+             
+              };
 
   ngOnInit() { 
     this.getCompanies();
+      
   }
   
   getCompanies(){
     this.companies = this._companyService.getCompanies();
-  //  this._companyService.getCompanies()
-  //      .subscribe( (companies):Company[] => 
-  //          this.companies = companies,
-  //          error=> this.errorMessage = <any>error 
-  //     );
-  
-  }
-
-  setClasses(company: Company) {
-    let red: boolean = (company.color === 'red');
-    let green = (company.color === 'green');
-    let blue = (company.color === 'blue');
-    let brown = (company.color === 'brown');
-    let yellow = (company.color === 'yellow');
-    let purple = (company.color === 'purple');
-
-    let classes = {
-      red: red,
-      green: green,
-      blue: blue,
-      brown: brown,
-      yellow: yellow,
-      purple: purple
-    };
-    return classes
-  }
-  
-  goToInvoice(company:Company) {
-    let uId = 1;
-    let coId = company.id;
-    this.invoice = this._invoiceService.makeInvoice(uId,coId);
-    this.router.navigate(['/invoice', this.invoice.id ]);
-
-  }
-
-  
-  goToCompanyDetails(company:Company){
-    let coId = company.id;
-    let coName = company.name;
-    let coHourly = company.hourly;
-    let coColor= company.color
-    let uId = 4
-    // console.log("coId:" + coId + " coColor:" + coColor  
-    //                                           + " coHourly:" + coHourly 
-    //                                           + " coName:" +coName 
-    //                                           + " uId:" + uId)
-    this.router.navigate(['/company-details', {
-                                               coId:coId,
-                                               coColor:coColor,
-                                               coHourly:coHourly, 
-                                               coName:coName, 
-                                               uId:uId,
-                                               }]);
-  }
-  goToCompanys(){
-    console.log("goToCompanys fired");
-      this.router.navigate(['/companys']);
-  }
-  goToCompanyCard(company){
-       let id = company.id;
-       this.router.navigate(['/company-card/ + id']);
   }
   
   goToEditCompany(company?:Company){
@@ -112,11 +56,5 @@ export class CompaniesComponent implements OnInit {
       } else {
           this.router.navigate(['/new-company']);
      }
-
-  }
-  goToNewItem(company){
-      let coName = company.name;
-      let coId = company.id;
-      this.router.navigate(['/new-item', {coName: coName, coId:coId }]);
   }
 }
