19ac63c04fe07769684c47d282e8f6093c09abc1
davidmgudeman <davidmgudeman@gmail.com>
2017-05-19 08:27:43 -0700

    register: onSubmit is loggin out

--- src/app/users/register/register.component.ts
+++ src/app/users/register/register.component.ts
@@ -8,6 +8,7 @@ import { FormBuilder,
 import { Router, ActivatedRoute } from '@angular/router';
 import { AlertService }           from '../services/alert.service';
 import { AuthenticationService }  from '../services/authentication.service';
+import { UserService }           from '../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -30,7 +31,8 @@ export class RegisterComponent implements OnInit {
         private _router: Router,
         private _authenticationService: AuthenticationService,
         private _alertService: AlertService,
-        private _fb:FormBuilder
+        private _fb:FormBuilder,
+        private _userService:UserService
         ) { }
 
     ngOnInit() {
@@ -46,7 +48,27 @@ export class RegisterComponent implements OnInit {
             "password2": this.fcPassword2,
         });
     }
+onSubmit() {
+      
+        let result;
+        // console.log (`x     ${x}`);
+        // console.log(`payload ${payload}`);
+        
+        // console.log(`HIIIIIIIIIII TTTTTTTTTTTTTT `)
+        // console.log(`this.fcType.value ${this.fcType.value}`);
+        let payload = this.myform.value;
+        console.log (`R payload     ${JSON.stringify(payload)}`);
+        // console.log (`IE x     ${JSON.stringify(x)}`);
+        
+           result = this._userService.addUser(payload);
+        //    console.log(`result on onSubmit in item-edit ${JSON.stringify(result)}`);
+        }
+        //    result.subscribe(x => {
+        //             // Ideally, here we'd want:
+        //             // this.form.markAsPristine();
+        //         this._router.navigate(['companies']);
+        //    });
+    }
 
 
 
-}
